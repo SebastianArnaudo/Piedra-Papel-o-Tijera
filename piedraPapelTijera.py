051b1071966dbd53408d9fc7cf1c46cb1b1714db
Sebastian Arnaudo <sebastianarnaudo.lamaruja@gmail.com>
2023-01-23 21:37:23 -0300

    finalizando juego

--- piedraPapelTijera.py
+++ piedraPapelTijera.py
@@ -10,110 +10,101 @@ def main():
 
     def piedra():
         
-        computadora = random.randint(0,4)
-        
-        ganador = ""
-
-        puntosJ = 0
-        puntosC = 0
-
-        eleccionJ= "Jugador eligio Piedra"
-        eleccionC=""
-
-        if computadora == 1:
-
-            ganador = "Empate"
-            eleccionC = "Computadora eligio Piedra"
-        elif computadora == 2:
-
-            ganador = "Gana Computador"
-            eleccionC = "Computadora eligio Papel"
-            puntosC += 1  
-        else:
-            ganador = "Gana Jugador"
-            eleccionC = "Computadora eligio Tijera"
-            puntosJ += 1
+            computadora = random.randint(0,4)
             
+            ganador = ""
+
+            empate = ""
         
-        puntosC = str(puntosC)
-        puntosJ = str(puntosJ)
-        
-        lJugadas["text"]= eleccionJ + "\n" + eleccionC
-        lGanadorRonda["text"] = ganador
-        lRondas["text"] = "Jugador " + puntosJ + ", " + " Computadora " + puntosC
+
+            eleccionJ= "Jugador eligio Piedra"
+            eleccionC=""
+
+            if computadora == 1:
+
+                empate = "Empate"
+                eleccionC = "Computadora eligio Piedra"
+            elif computadora == 2:
+
+                ganador = "Gana Computador"
+                eleccionC = "Computadora eligio Papel"
+                
+            else:
+                ganador = "Gana Jugador"
+                eleccionC = "Computadora eligio Tijera"
+            
+            lJugadas["text"]= eleccionJ + "\n" + eleccionC
+            lGanador["text"] = ganador
+            lEmpate["text"] = empate
+            
 
     def papel():
 
-        computadora = random.randint(0,4)
-        
-        ganador = ""
-
-        puntosJ = 0
-        puntosC = 0
-
-        eleccionJ= "Jugador eligio Papel"
-        eleccionC=""
-
-        if computadora == 1:
-
-            ganador = "Gana Jugador"
-            eleccionC = "Computadora eligio Piedra"
-            puntosJ += 1
-
+            computadora = random.randint(0,4)
             
-        elif computadora == 2:
+            ganador = ""
 
-            ganador = "Empate"
-            eleccionC = "Computadora eligio Papel"
-           
-        else:
-            ganador = "Gana Computadora"
-            eleccionC = "Computadora eligio Tijera"
-            puntosC += 1
+            empate = ""
+
+
+            eleccionJ= "Jugador eligio Papel"
+            eleccionC=""
+
+            if computadora == 1:
+
+                ganador = "Gana Jugador"
+                eleccionC = "Computadora eligio Piedra"
+                
+
+                
+            elif computadora == 2:
+
+                empate = "Empate"
+                eleccionC = "Computadora eligio Papel"
             
-        
-        puntosC = str(puntosC)
-        puntosJ = str(puntosJ)
-
-        lJugadas["text"]= eleccionJ + "\n" + eleccionC
-        lGanadorRonda["text"] = ganador
-        lRondas["text"] = "Jugador " + puntosJ + ", " + " Computadora " + puntosC
+            else:
+                ganador = "Gana Computadora"
+                eleccionC = "Computadora eligio Tijera"
+                
+            lJugadas["text"]= eleccionJ + "\n" + eleccionC
+            lGanador["text"] = ganador
+            lEmpate["text"] = empate
 
     def tijera():
-        
-        computadora = random.randint(0,4)
-        
-        ganador = ""
-
-        puntosJ = 0
-        puntosC = 0
-
-        eleccionJ= "Jugador eligio Tijera"
-        eleccionC=""
-
-        if computadora == 1:
-
-            ganador = "Gana Computadora"
-            eleccionC = "Computadora eligio Piedra"
-            puntosC += 1
-        elif computadora == 2:
-
-            ganador = "Gana Jugador"
-            eleccionC = "Computadora eligio Papel"
-            puntosJ += 1           
-        else:
             
-            ganador = "Empate"
-            eleccionC = "Computadora eligio Tijera"
+            computadora = random.randint(0,4)
             
-        
-        puntosC = str(puntosC)
-        puntosJ = str(puntosJ)
+            ganador = ""
 
-        lJugadas["text"]= eleccionJ + "\n" + eleccionC
-        lGanadorRonda["text"] = ganador
-        lRondas["text"] = "Jugador " + puntosJ + ", " + " Computadora " + puntosC   
+            empate = ""
 
+            eleccionJ= "Jugador eligio Tijera"
+            eleccionC=""
+
+            if computadora == 1:
+
+                ganador = "Gana Computadora"
+                eleccionC = "Computadora eligio Piedra"
+                
+            elif computadora == 2:
+
+                ganador = "Gana Jugador"
+                eleccionC = "Computadora eligio Papel"
+                        
+            else:
+                
+                empate = "Empate"
+                eleccionC = "Computadora eligio Tijera"
+                
+            
+            
+
+            lJugadas["text"] = eleccionJ + "\n" + eleccionC
+            lGanador["text"] = ganador
+            lEmpate["text"] = empate
+            
+
+    
 
 
     framePrincipal=tk.Frame(ventana,bg="Gray")
@@ -124,25 +115,24 @@ def main():
     lBienvenida.place(x=100, y=10)
 
     bPiedra=tk.Button(framePrincipal,text="Piedra",bg="Green1", command= piedra)
-    bPiedra.place(x=90,y=60)
+    bPiedra.place(x=100,y=60)
 
     bPapel=tk.Button(framePrincipal,text="Papel",bg="Red1",fg="White",command= papel)
-    bPapel.place(x=140,y=60)
+    bPapel.place(x=150,y=60)
 
     bTijera=tk.Button(framePrincipal,text="Tijera",bg="Blue1",fg="white", command= tijera)
-    bTijera.place(x=115,y=90)
+    bTijera.place(x=125,y=90)
 
     lJugadas=tk.Label(framePrincipal,text="",bg="Gray")
     lJugadas.place(x=65,y=150)
 
-    lGanadorRonda=tk.Label(framePrincipal,text="",bg="Gray")
-    lGanadorRonda.place(x=80,y=200)
-
-    lRondas=tk.Label(framePrincipal,text="",bg="Gray")
-    lRondas.place(x=70,y=250)
-
     lGanador=tk.Label(framePrincipal,text="",bg="Gray")
-    lGanador.place(x=75,y=300)
+    lGanador.place(x=90,y=200)
+
+    lEmpate=tk.Label(framePrincipal,text="",bg="Gray")
+    lEmpate.place(x=120, y=230)
+    
+
 
 
 
